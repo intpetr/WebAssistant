60b77f914772625a8cff2c15bccfa9f6e59adc7a
Ignát Péter <ignat.peti@gmail.com>
2025-10-11 18:59:12 +0200

    Fix for lack of js client support

--- Backend/main.py
+++ Backend/main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from flask_sqlalchemy import SQLAlchemy
 
@@ -6,6 +7,11 @@ from Models import User, db
 
 app = Flask(__name__)
 app.secret_key = "supersecretkey"
+
+CORS(app)
+#import logging
+#log = logging.getLogger('werkzeug')
+#log.setLevel(logging.ERROR)
 
 
 login_manager = LoginManager()
@@ -34,13 +40,17 @@ def register():
     db.session.add(new_user)
     db.session.commit()
 
+    print('Successfully registered user:',username, 'with password:', password)
+
     return jsonify({"message": f"User {username} registered successfully"}), 201
 
 
 
 @app.route('/login', methods=['POST'])
 def login():
+
     data = request.get_json()
+    print(data)
     username = data.get('username')
     password = data.get('password')
 
@@ -49,12 +59,15 @@ def login():
         return jsonify({"error": "Invalid credentials"}), 401
 
     login_user(user)
+    print('User successfully logged in :', username)
     return jsonify({"message": "Login successful", "user": username}), 200
 
 
 @app.route('/dashboard', methods=['GET'])
 @login_required
 def dashboard():
+
+    print('Dashboard returned to user')
     return jsonify({"message": f"Hello {current_user.username}, welcome to your dashboard!"})
 
 
@@ -62,15 +75,17 @@ def dashboard():
 @login_required
 def logout():
     logout_user()
+    print('user logged out')
     return jsonify({"message": "Logged out successfully"}), 200
 
 
 if __name__ == "__main__":
-    with open('credentials.txt') as file:
+    with open('C:/Users/ignat/Desktop/Aktív/assistantfolder/WebAssistant/Backend/credentials.txt') as file:
         dbstring = file.readline()
         app.config[
             'SQLALCHEMY_DATABASE_URI'] = dbstring
         app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
         db.init_app(app)
 
-    app.run(debug=True)
+    app.run()
+    print('running')
