8cc0451a125e916fa818ec1c3bddc5aa402b3b90
Nightmer71 <totharon71@gmail.com>
2025-10-17 11:57:09 +0200

    Added the DOM elements to the Setting.js

--- Front-end/Settings/Settings.js
+++ Front-end/Settings/Settings.js
@@ -1,2 +1,13 @@
+/* DOM */
+const saveButton = document.getElementById("save-button");
+const weatherApiCheckBox = document.getElementById("api-weather");
+const currencyApiCheckBox = document.getElementById("api-currency");
+const stockApiCheckBox = document.getElementById("api-stock");
+const newsApiCheckBox = document.getElementById("api-news");
+const memeApiCheckBox = document.getElementById("api-meme");
+const prayerApiCheckBox = document.getElementById("api-prayer");
+const ticketApiCheckBox = document.getElementById("api-flight");
+const moonApiCheckBox = document.getElementById("api-moon");
+
 
 
