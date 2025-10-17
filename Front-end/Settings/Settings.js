bfb394a9d59c069626ba36b8bf71ae88b0277ab7
Nightmer71 <totharon71@gmail.com>
2025-10-17 14:48:39 +0200

    Added the fetching of existing preferences

--- Front-end/Settings/Settings.js
+++ Front-end/Settings/Settings.js
@@ -1,6 +1,7 @@
 /* Configuration */
 const API_BASE_URL = 'http://localhost:5000';
 const SAVE_ENDPOINT = `${API_BASE_URL}/api/settings`;
+const LOAD_ENDPOINT = `${API_BASE_URL}/api/settings`;
 
 /* DOM Element References */
 const settingsForm = document.getElementById('settings-form');
@@ -46,3 +47,57 @@ function setButtonState(button, isLoading, originalText){
 
 /* --- Core Logic --- */
 
+/* Fetching the existing preferences of the user */
+async function loadInitialPreferences() {
+    const allCheckBoxes = document.querySelectorAll('api_preference');
+
+    // Checking all boxes for new users or load failiure
+    allCheckBoxes.forEach(checkbox => {
+        checkbox.checked = true;
+    })
+
+    try{
+        const response = await fetch(LOAD_ENDPOINT, {
+            method: 'GET',
+            headers: {
+                'Content Type' : 'application/json',
+            },
+        });
+
+        if (response.ok){
+            const data = await response.json();
+            const userSettings = data.settings || {};
+            const enabledAPIs = userSettings.enabledApis || [];
+            
+            // Only proceeding if the server returned saved data
+            if (enabledAPIs.length > 0){
+                allCheckBoxes.forEach(checkbox => {
+                    checkbox.checked = false;
+                })
+
+                enabledAPIs.forEach(apiValue => {
+                    const checkbox = document.getElementById(`api-${apiValue}`);
+                    if (checkbox){
+                        checkbox.checked = true;
+                    }});}
+
+                else{
+                    console.log('User has saved settings, but the list is empty. Showing all checked by default.')
+                }
+            }
+        else if (response.status === 404 || response.status === 401){
+            console.warn('Could not load preferences. Defaulting to all checked');
+        }
+        else{
+            console.error(`Failed to load the preferences with status: ${response.status}`);
+            showMessage('Error loading saved preferences.', 'error');
+        }
+}
+    catch(error){
+        console.error(`Network error during initial load. Defaulting to all checked: ${error}`);
+    }
+}
+
+/* Handling form submission. 
+    Collecting checked APIs, formating JSON, and sending to backend
+*/
