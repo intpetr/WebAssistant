62ea36f05c1d5b16851ce28426c78c5f5254e80c
Nightmer71 <totharon71@gmail.com>
2025-11-08 12:40:01 +0100

    Added the UI Utility functions to Note.js

--- Front-end/Notes/Notes.js
+++ Front-end/Notes/Notes.js
@@ -14,3 +14,55 @@ const noteTitleInput = document.getElementById("note-title");
 const noteContentInput = document.getElementById("note-content");
 const messageBox = document.getElementById("message-box");
 const messageText = document.getElementById("message-text");
+
+/* Global state for storing all fetched notes */
+let allNotes = [];
+
+/* --UI Utility functions-- */
+/* Showing error / success message to the user */
+function showMessage(message, type = "error") {
+        messageBox.classList.remove(
+                "hidden",
+                "message-success",
+                "message-error"
+        );
+        messageBox.classList.add(`message-${type}`);
+        messageText.textContent = message;
+
+        setTimeout(() => {
+                messageBox.classList.add("hidden");
+                messageText.textContent = "";
+        }, 5000);
+}
+
+/* Setting the save button to a loading state or back to normal */
+function setButtonState(button, isLoading, originalText) {
+        if (isLoading) {
+                button.disabled = true;
+                button.innerHTML =
+                        '<span class="loading-spinner"><span/> Saving...';
+        } else {
+                button.disabled = false;
+                button.innerHTML = originalText;
+        }
+}
+
+/* Clearing the note and resetting the UI state */
+function clearForm() {
+        noteForm.reset();
+        noteIdInput.value = "";
+        noteForm.classList.add("hidden");
+        saveNoteBtn.innerHTML =
+                '<i class="fa-solid fa-floppy-disk"><i/> Save Note';
+}
+
+/* Populating the form with existing note date for editing */
+function editNote(note) {
+        noteIdInput.value = note.id;
+        noteTitleInput.value = note.title;
+        noteContentInput.value = note.content;
+
+        noteForm.classList.remove("hidden");
+        saveNoteBtn.innerHTML =
+                '<i class="fa-solid fa-pencil"><i/> Update Note';
+}
